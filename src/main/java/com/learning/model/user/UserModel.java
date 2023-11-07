5b2454620c81a8fbb125f56f2edd96c7e1174155
Adimaryadi <adimaryaditech@gmail.com>
2023-11-07 11:43:22 +0700

    update version use java 17 update field db model user

--- src/main/java/com/learning/model/user/UserModel.java
+++ src/main/java/com/learning/model/user/UserModel.java
@@ -13,15 +13,23 @@ public class UserModel {
     public UserModel() {};
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
 
-    @Column(name = "name")
+    @Column(name = "name", columnDefinition = "VARCHAR(50)")
     private String name;
+
+    @Column(name = "username", columnDefinition = "VARCHAR(10)")
+    private String username;
 
     @Column(name = "email")
     private String email;
 
-    public UserModel(String name, String email) {
+    @Column(name = "password", columnDefinition = "TEXT")
+    private String password;
+
+    public UserModel(String name, String email, String username, String password) {
         this.name = name;
         this.email = email;
+        this.username = username;
+        this.password = password;
     }
 
     public Long getId() {
@@ -40,11 +48,27 @@ public class UserModel {
         this.name = name;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
     }
 }
